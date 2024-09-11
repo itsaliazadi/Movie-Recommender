419cbd88eb3ec8a4e16d6f3b5ef0f993960fa3b4
Ali Azadi <aliazadicode@gmail.com>
2024-09-11 19:36:32 +0330

    Added a functionality to the code in case the user's movie was not found.

--- Recommendation.py
+++ Recommendation.py
@@ -5,7 +5,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 
-df = pd.read_csv("movies.csv")
+df = pd.read_csv("C:\\Users\\User\\Desktop\\Movie Recommender\\Movie-Recommender\\movies.csv")
 df["data"] = df["title"] + " " + df["description"]
 
 vec = TfidfVectorizer()
@@ -20,17 +20,25 @@ def recommend(movie_title):
     closest_match = process.extractOne(movie_title, list(df["title"]))
     if closest_match[1] > 90:
         movie = closest_match[0]
-        
-    movie_index = df[df["title"] == movie].index[0]
-
-    scores = list(enumerate(similarity[movie_index]))
-    sorted_scores = sorted(scores, key=lambda x:x[1], reverse=True)[1:]
-    print(sorted_scores)
     
-    movie_recommendations = list()
+    try:
+        
+        movie_index = df[df["title"] == movie].index[0]
 
-    for index, score in sorted_scores:
-        recommendation = df.iloc[index]["title"]
-        movie_recommendations.append(recommendation)
+        scores = list(enumerate(similarity[movie_index]))
+        sorted_scores = sorted(scores, key=lambda x:x[1], reverse=True)[1:]
+        print(sorted_scores)
+        
+        movie_recommendations = list()
 
-    return movie_recommendations
+        for index, score in sorted_scores:
+            recommendation = df.iloc[index]["title"]
+            movie_recommendations.append(recommendation)
+
+        return movie_recommendations
+    
+    except:
+        
+        movie_recommendations = list()
+
+        return movie_recommendations
